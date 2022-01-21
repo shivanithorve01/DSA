2ad1f991baafb2993242f41c0367521a9adbdd00
Shivani Thorve <shivanithorve01@gmail.com>
2022-01-21 11:07:37 +0530

    Simple Brute Force Approach for LargestSum of Contiguous Subarray

---
+++ 1.Arrays/8.1.Largest Sum of Contiguous Subarray (Brute Force).cpp
@@ -0,0 +1,91 @@
+/******************************************************************************
+LARGEST SUM OF CONTIGUOUS SUBARRAY 
+            
+Problem Statement = Given an array A[] with n elements.find the maximum sum of a subarray 
+among all subarrays of that array. A subarray of array A[] of length n is a contiguous segment from A[i] through A[j]
+where 0<= i <= j <= n. Some properties of this problem are:
+    If the array contains all positive numbers, the maximum subarray is the entire array.
+    Several different sub-arrays may have the same maximum sum.
+    
+Example :
+    1) A = {2,1,-2,5,6,-7,3}
+       Subarray = {5,6} 
+       o/p Largest Sum = 11
+       
+    2) B = {7,2,3,1,10}
+       Subarray = {7,2,3,1,10}... if all nos are positive then their sum is largest sum.
+       o/p largest Sum = 23
+       
+    3) C = {-2}...only one negative number
+        Subarray = {-2}
+        o/p Largest Sum = -2
+        
+    4) D = {-9,-4,-3,-7,-2,-10} ...if all elements are negative then largest sum should be largest element in array
+        Subarray = {-2}
+        o/p Largest Sum = -2
+        
+Simple Approach : Brute Force
+    Find all possible subarrays and their sum.Largest among them is required largest sum.
+    1) Initialise max_sum=A[0]
+    2) starting from every element,make subarrays using 2 nested for loops
+    3) for 1st loop, i=0 to i=n-1
+    4) Initialize curr_sum = 0
+    5) for inner for loop,j=i to j=n-1
+    6) add A[j] to curr_sum for every pass.
+    7) if curr_sum > max_sum, then max_sum=curr_sum
+    8) return max_sum
+
+Time Complexity : O(n^2)
+Drawbacks :
+1)Time Complexity is very large 
+
+Best Approach : Kadane's Algorithm with Time Complexity = O(n)
+Tip : Don't use sorting technique as in order to find subarray, order of an array becomes important.
+      Time Complexity will reduce in Divide & Conquer Method to O(nlogn) but space complexity will increase 
+      to O(nlogn)
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+//Function
+void largestSum (int Arr[], int n)
+{
+    //if we got only one negative no. in an array then largest sum should be no. itself
+    //if we got all negatives then sum should be largest element among all
+    int max_sum = Arr[0] ; 
+    for(int i=0;i<n;i++)
+    {
+        int curr_sum = 0;
+        for (int j=i;j<n;j++)
+        {
+            curr_sum = curr_sum + Arr[j];
+            if(curr_sum > max_sum)
+            {
+                max_sum = curr_sum;
+            }
+        }
+    }
+    cout<<max_sum<<" ";
+}
+
+//Main Function
+int main ()
+{
+  int A[] = { 2, 1, -2, 5, 6, -7, 3 };
+  int a = sizeof(A)/sizeof(A[0]);
+  int B[] = {7,2,3,1,10};
+  int b = sizeof(B)/sizeof(B[0]);
+  int C[] = {-2};
+  int c = sizeof(C)/sizeof(C[0]);
+  int D[] = {-9,-4,-3,-7,-2,-10};
+  int d = sizeof(D)/sizeof(D[0]);
+  
+  cout<<"\nLargest Sum of subarray of A :";
+  largestSum(A,a);
+  cout<<"\nLargest Sum of subarray of B :";
+  largestSum(B,b);
+  cout<<"\nLargest Sum of subarray of C :";
+  largestSum(C,c);
+  cout<<"\nLargest Sum of subarray of D :";
+  largestSum(D,d);
+  return 0;
+}
